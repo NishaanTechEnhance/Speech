d52ec23f9053a54d881980a123ce8ea8ac022045
jaswanthte <jaswanth.sabbineni@techenhance.com>
2024-07-30 23:53:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173", "https://gentle-cliff-023d4d00f.5.azurestaticapps.net"])
+app = Flask(__name__)  # Creating a Flask application instance
+CORS(app)  # Enable CORS for all routes
 
 # Register blueprints here
-from routes import routes
-app.register_blueprint(routes)
+from routes import routes  # Importing the 'routes' blueprint from routes.py
+app.register_blueprint(routes)  # Registering the 'routes' blueprint with the Flask application
 
 @app.route('/')
 def index():
-    return "Flask server is running!"
+    return "Flask server is runningg!"  # A simple route to check if the server is running
 
 if __name__ == '__main__':
+    # Running the Flask application on host '0.0.0.0' and port 80
     app.run(host='0.0.0.0', port=80, debug=True)
